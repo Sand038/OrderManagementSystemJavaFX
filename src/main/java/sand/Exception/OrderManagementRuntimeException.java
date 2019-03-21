73fa351f7499eb1f58a8c0dce143e6fbb630ad02
Sand038 <sandeepamadhusanka038@gmail.com>
2019-03-21 19:54:42 +0530

    Adding exceptions to OM package

---
+++ src/main/java/sand/Exception/OrderManagementRuntimeException.java
@@ -0,0 +1,9 @@
+package sand.Exception;
+
+public class OrderManagementRuntimeException extends RuntimeException
+{
+  public OrderManagementRuntimeException(String message, Throwable cause)
+  {
+    super(message, cause);
+  }
+}
